5adae7a6a4a0a7a30d669bdcfd6f39de5b11d822
MrNoize <adelproteck@gmail.com>
2021-05-23 17:27:14 +0300

    Добавил функционал запроса уведомления о необходимой карте на сервере. Планы: сделать автоочистку и логирование запросов, которым больше 3 дней и они все еще не были поставлены на сервер.

--- bot/petbot/pr_kotleta.py
+++ bot/petbot/pr_kotleta.py
@@ -2,13 +2,17 @@ import discord
 import psycopg2
 from psycopg2 import sql
 from discord.ext import commands, tasks
-import os, sys
+from datetime import datetime
+import os
 import pytz
 import pickle
+import json
 import re
 
 
-def load_config(config_name, params=[]):
+def load_config(config_name, params=None):
+    if params is None:
+        params = []
     import configparser
     configs = configparser.ConfigParser()
     configs.read('../configs/config.ini')
@@ -23,12 +27,52 @@ def load_config(config_name, params=[]):
     return output
 
 
+def read_maplist():
+    with open("../temp/map_requests.json", "r", encoding="utf-8") as map_requests_json_read:
+        return json.load(map_requests_json_read)[0]
+
+
+def add_to_mapreq_json(data):
+    with open("../temp/map_requests.json", "r", encoding="utf-8") as map_requests_json_read:
+        exist_data = json.load(map_requests_json_read)
+    with open("../temp/map_requests.json", "w", encoding="utf-8") as map_requests_json_write:
+        readen_dict = exist_data[1]['requests']
+        for request in exist_data[1]['requests']:
+            if data['Nick'] == request['Nick']:
+                readen_dict.remove(request)
+        readen_dict.append(data)
+        json.dump(exist_data, map_requests_json_write)
+
+
+def delete_from_mapreq_json(data):
+    with open("../temp/map_requests.json", "r", encoding="utf-8") as map_requests_json_read:
+        exist_data = json.load(map_requests_json_read)
+    with open("../temp/map_requests.json", "w", encoding="utf-8") as map_requests_json_write:
+        readen_dict = exist_data[1]['requests']
+        readen_dict.remove(data)
+        json.dump(exist_data, map_requests_json_write)
+
+
+async def check_mapreq(current_map):
+    with open("../temp/map_requests.json", "r", encoding="utf-8") as map_requests_json_read:
+        requests_list = json.load(map_requests_json_read)[1]['requests']
+    for request in requests_list:
+        if current_map.lower() == request['Map']:
+            await bot.get_user(int(request['Player'])).send(f"You've requested map {request['Map']}.\n"
+                                                            "It is on Russian reality right now.")
+            delete_from_mapreq_json(request)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
 bot_config = load_config("bot", ["TOKEN", "GUILD", "CHANNEL", "CHECK_DELAY", "BOT_CHANNEL"])
 db_config = load_config("database", ["dbname", "table_name", "user", "password", "host"])
 TOKEN = bot_config['TOKEN']
 GUILD = os.getenv(bot_config['GUILD'])
-bot = commands.Bot(command_prefix='!')
-client = discord.Client()
+bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
+maplist = read_maplist()
 
 bot.remove_command('help')
 
@@ -41,7 +85,7 @@ async def get_stats(manualy=False):
     cursor = conn.cursor()
     cursor.execute(
         sql.SQL("SELECT map, g_mode, players, time FROM {} ORDER BY id DESC LIMIT 1")
-        .format(sql.Identifier(db_config['table_name'])))
+            .format(sql.Identifier(db_config['table_name'])))
     print("Mapstats successfully readen from DB")
     rows = cursor.fetchall()
     for row in rows:
@@ -53,12 +97,13 @@ async def get_stats(manualy=False):
     conn.close()
     if int(players_online) > 20 or manualy:
         send_message = ('------------------------\n'
-                        f'Time: {time}. \n'                        
+                        f'Time: {time}. \n'
                         f'Map: {map_name}. \n'
                         f'Game mode: {game_mode}. \n'
                         f'Players online: {players_online} \n'
                         '------------------------')
         await bot.get_channel(int(bot_config['CHANNEL'])).send(send_message)
+    await check_mapreq(map_name)
 
 
 async def auto_refresh():
@@ -72,7 +117,8 @@ async def is_online(nickname: str):
     message_to_sent = "I've got next result:\n"
     if isinstance(response, list):
         for player in response:
-            message_to_sent = message_to_sent + f"""Player: {player['Player']} \nOn server: {player['Server']} \n"""
+            message_to_sent = message_to_sent + f"""Player: {player['Player']} \n
+                                                On server: {player['Server']} \n"""
     elif isinstance(response, str):
         message_to_sent = response
     return message_to_sent
@@ -138,5 +184,24 @@ async def s(ctx, message=""):
         await ctx.message.author.send("You should give me ID")
     else:
         await ctx.message.author.send(await p_stats(int(pid[0])))
+
+
+@bot.command(pass_context=True)
+async def req(ctx):
+    await ctx.message.delete()
+    mapname = ctx.message.content.strip('!req ')
+    search_result = []
+    for item in maplist:
+        if mapname.lower() in item and not mapname == "":
+            search_result.append(item)
+    if len(search_result) == 1:
+        add_to_mapreq_json({"Player": ctx.message.author.id, "Nick": str(ctx.message.author.name), "Map": search_result[0], "Time": str(datetime.date(datetime.now(pytz.timezone('Europe/Moscow'))))})
+        await ctx.message.author.send(f"I'll type you when {search_result[0]} will set on Russian Reality.")
+    elif len(search_result) == 0:
+        await ctx.message.author.send(f"Can't find any maps on request: {mapname}")
+    else:
+        await ctx.message.author.send(f"I found several maps on your request. Please, extend your request.\n"
+                                      f" Maps on your request: {', '.join(search_result)}")
+
 
 bot.run(TOKEN)
